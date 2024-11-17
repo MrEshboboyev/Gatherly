2250db58b917c17d7e5b3a28557a291940a84248
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-17 23:55:46 +0500

    Entities, DomainEvents, Enums, Errors, Exceptions, ValueObjects,

---
+++ Gatherly.Domain/DomainEvents/InvitationAcceptedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Gatherly.Domain.Primitives;
+
+namespace Gatherly.Domain.DomainEvents;
+
+public sealed record InvitationAcceptedDomainEvent(Guid InvitationId, Guid GatheringId) : 
+    IDomainEvent
+{
+
+}
