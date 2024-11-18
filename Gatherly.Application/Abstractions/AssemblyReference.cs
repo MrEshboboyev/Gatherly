7f79d3325f8359cab9b31458501e62b6284fc08c
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-18 11:07:05 +0500

    Email Service interface and Assembly Reference

---
+++ Gatherly.Application/Abstractions/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Gatherly.Application.Abstractions;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly assembly = typeof(AssemblyReference).Assembly;
+}
