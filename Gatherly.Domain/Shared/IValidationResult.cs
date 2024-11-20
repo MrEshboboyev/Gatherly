b7bcb87b3532a8fdfa67fb3fb3c21d227bb08bab
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-20 12:00:11 +0500

    ValidationResult : interface and generic

---
+++ Gatherly.Domain/Shared/IValidationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Gatherly.Domain.Shared;
+
+public interface IValidationResult
+{
+    public static readonly Error ValidationError = new(
+        "ValidationError",
+        "A vaidation problem occurred!");
+
+    Error[] Errors { get; }
+}
