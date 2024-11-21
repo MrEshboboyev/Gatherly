b08fedf3ec12b69a02fc5ccd68751ff3774d9552
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-21 17:52:10 +0500

    Add specification pattern implementation

--- Gatherly.Domain/Repositories/IGatheringRepository.cs
+++ Gatherly.Domain/Repositories/IGatheringRepository.cs
@@ -11,8 +11,9 @@ public interface IRepository<T>
 
 public interface IGatheringRepository : IRepository<Gathering>
 {
-    Task<List<Gathering>> GetByCreatorIdAsync(Guid creatorId, CancellationToken cancellationToken = default);
+    Task<List<Gathering>> GetByNameAsync(string name, CancellationToken cancellationToken = default);
     Task<Gathering> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<Gathering>> GetByCreatorIdAsync(Guid creatorId, CancellationToken cancellationToken = default);
     Task<Gathering> GetByIdWithCreatorAsync(Guid id, CancellationToken cancellationToken = default);
     Task<Gathering> GetByIdWithInvitationsAsync(Guid id, CancellationToken cancellationToken = default);
     void Add(Gathering gathering);
--- Gatherly.Persistence/Repositories/GatheringRepository.cs
+++ Gatherly.Persistence/Repositories/GatheringRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Gatherly.Domain.Entities;
 using Gatherly.Domain.Repositories;
+using Gatherly.Persistence.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gatherly.Persistence.Repositories;
@@ -11,6 +12,28 @@ public sealed class GatheringRepository : IGatheringRepository
     {
         _dbContext = dbContext;
     }
+
+    public async Task<List<Gathering>> GetByNameAsync(
+            string name,
+            CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Set<Gathering>()
+             .Include(gathering => gathering.Creator)
+             .Include(gathering => gathering.Attendees)
+             .Where(gathering =>
+                  string.IsNullOrEmpty(name) ||
+                  gathering.Name.Contains(name))
+             .OrderBy(gathering => gathering.Name)
+             .ToListAsync(cancellationToken);
+    }
+
+    //public async Task<List<Gathering>> GetByNameAsync(
+    //     string name,
+    //     CancellationToken cancellationToken = default)
+    //{
+    //    return await ApplySpecification(new GatheringByNameSpecification(name))
+    //        .ToListAsync(cancellationToken);
+    //}
 
     public async Task<List<Gathering>> GetByCreatorIdAsync(
             Guid creatorId,
@@ -39,24 +62,44 @@ public sealed class GatheringRepository : IGatheringRepository
         return gathering;
     }
 
+    //public async Task<Gathering?> GetByIdAsync(
+    //    Guid id,
+    //    CancellationToken cancellationToken = default) =>
+    //    await ApplySpecification(new GatheringByIdSplitSpecification(id))
+    //      .FirstOrDefaultAsync(cancellationToken);
+
     public async Task<Gathering> GetByIdWithCreatorAsync(Guid id, CancellationToken cancellationToken = default)
     {
         return await _dbContext.Set<Gathering>()
             .Include(x => x.Creator)
             .FirstOrDefaultAsync(g => g.Id == id, cancellationToken);
     }
+
+    //public async Task<Gathering> GetByIdWithCreatorAsync(
+    //    Guid id,
+    //    CancellationToken cancellationToken = default) =>
+    //    await ApplySpecification(new GatheringByIdWithCreatorSpecification(id))
+    //        .FirstOrDefaultAsync(cancellationToken);
+
+
     public async Task<Gathering> GetByIdWithInvitationsAsync(Guid id, CancellationToken cancellationToken = default)
     {
         return await _dbContext.Set<Gathering>()
             .Include(x => x.Invitations)
             .FirstOrDefaultAsync(g => g.Id == id, cancellationToken);
     }
-    public void Add(Gathering gathering)
+
+    private IQueryable<Gathering> ApplySpecification(
+            Specification<Gathering> specification)
     {
+        return SpecificationEvaluator.GetQuery(
+            _dbContext.Set<Gathering>(),
+            specification);
+    }
+
+    public void Add(Gathering gathering) =>
         _dbContext.Set<Gathering>().Add(gathering);
-    }
-    public void Remove(Gathering gathering)
-    {
+
+    public void Remove(Gathering gathering) =>
         _dbContext.Set<Gathering>().Remove(gathering);
-    }
 }
